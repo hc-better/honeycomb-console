a94cd11f630db873e4b91e4bb1c542225b01df8c
XQ0118 <173141178@qq.com>
2020-07-04 14:54:23 +0800

    feat:授权列表-添加&更新&删除

--- assets/src/pages/cluster-auth/auth-add.jsx
+++ assets/src/pages/cluster-auth/auth-add.jsx
@@ -6,7 +6,6 @@ import {Modal, Form, Input, message, Select} from 'antd';
 import notification from '@coms/notification';
 import _ from 'lodash';
 import {tryParse, tryArrToStr} from '@lib/util';
-
 import {aclApi} from '@api';
 
 const layout = {
@@ -125,7 +124,6 @@ const AuthAdd = (props) => {
           <Select style={{width: 120}}>
             <Select.Option value={1}>管理员</Select.Option>
             <Select.Option value={0}>普通用户</Select.Option>
-
           </Select>
         </Form.Item>
         <Form.Item
